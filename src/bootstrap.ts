46ca1dda8a07b00b043b39d52b9206157dd26401
onlyjackfrost <onlyjackfrost@gmail.com>
2024-10-15 00:57:55 +0800

    fix lint issue

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -17,4 +17,5 @@ const eventConsumer = new EventConsumer({
 });
 eventConsumer.start();
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { eventQueue, eventRepository, eventConsumer, knex };
--- src/pages/monitor.js
+++ src/pages/monitor.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function MonitorPage() {
   const [startDate, setStartDate] = useState('');
