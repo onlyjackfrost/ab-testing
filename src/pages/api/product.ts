253ead5476ad9d2e962bbe01a236f6c0c9f8337c
onlyjackfrost <onlyjackfrost@gmail.com>
2024-10-18 00:06:03 +0800

    update decision making

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -11,26 +11,31 @@ const getAssignedKey = (productLength: number) => {
   return mod;
 };
 
+// How to send consistent result to the client:
 // There are ways to memorize a user's is using which test, like session.
 // There are trade-off between session and cookie. The choice depends on the business goals.
 // but for demo purpose, I just use the cookie to memorize the test name.
 
+// About the API design:
 // I design this API router only for demo.
 // In reality, we might have to make this API general
 // and we should know which page content should be rendered from the request(eg. query path, query params, header...)
 // then use this identifier to find the content.
+
+// About the random logic:
+// There are many ways to decide how to assign the product,
+// If userId was provided, we can use it to avoid the same user trying to get advantage by clearing the cookie (using session could avoid this as well)
+// If we don't have to distribute the test strictly even, we can use a simple random assign method.
+// The assign rules should be decided based on the business goals.
+// Here I choose the simplest way, use running integer to assign the product to have a better demo effect.
 export default function handler(req, res) {
   let productHeader = req.cookies["ab-product"];
+  // leave the console.log for demo purpose
   console.log(`productHeader: ${productHeader}`);
 
   // if no product header or not in the product list, assign a new product
   const testNames = products.map((product) => product.getTestName());
   if (!productHeader || !testNames.includes(productHeader)) {
-    // There are many ways to decide how to assign the product,
-    // If userId was provided, we can use it to avoid the same user trying to get advantage by clearing the cookie (using session could avoid this as well)
-    // If we don't have to distribute the test strictly even, we can use a simple random assign method.
-    // The assign rules should be decided based on the business goals.
-    // Here I choose the simplest way, use running integer to assign the product to have a better demo effect.
     productHeader = products[getAssignedKey(testNames.length)].getTestName();
 
     console.log(`assign new product: ${productHeader}`);
